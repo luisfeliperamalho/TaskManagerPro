258369ba7eabeaaf168911edbc97908b104fd5e7
Luis Felipe Ramalho Oliveira <luis_felipero@hotmail.com>
2025-06-24 12:40:28 -0300

    new task model in infrastructure

---
+++ TaskManager.Infrastructure/Models/TaskModel.cs
@@ -0,0 +1,10 @@
+namespace TaskManagerPro.Infrastructure.Models;
+
+public class TaskModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+}
